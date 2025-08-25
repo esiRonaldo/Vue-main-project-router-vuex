91f97558ad9bc4b822b9356d1f10822d6cd4f49d
Ehsan Fani <ehsan.fani@emeal.nttdata.com>
2025-08-25 12:34:00 +0200

    Added router

---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>CoachRegisteration</div>
+</template>
---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import NotFound from './pages/NotFound.vue';
+import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
+import CoachRegisteration from './pages/coaches/CoachRegisteration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import Coacheslist from './pages/coaches/Coacheslist.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: Coacheslist },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: '/contact', component: ContactCoach }],
+    },
+    { path: '/register', component: CoachRegisteration },
+    { path: '/requests', component: RequestsRecieved },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
